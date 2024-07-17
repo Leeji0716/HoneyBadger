d8c44f3edb64fc700aa36e6e50384854048404d1
taehun7497 <kth7497@gmail.com>
2024-07-17 13:11:11 +0900

    commit

--- backend/src/main/java/com/team/HoneyBadger/Service/MultiService.java
+++ backend/src/main/java/com/team/HoneyBadger/Service/MultiService.java
@@ -952,14 +952,6 @@ public class MultiService {
         }
     }
 
-
-   public List<String> getMessage(Long id) {
-        Message message = messageService.getMessageById(id);
-        List<String> users = message.getReadUsers();
-
-        return users;
-    }
-
     /*
      * Department
      */
