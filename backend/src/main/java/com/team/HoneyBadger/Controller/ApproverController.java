453f24363914bf980d4509e31e01dd137c86e8ea
ãDowonNam <dwnam0114@gmail.com>
2024-07-24 10:21:51 +0900

    add the read

--- backend/src/main/java/com/team/HoneyBadger/Controller/ApproverController.java
+++ backend/src/main/java/com/team/HoneyBadger/Controller/ApproverController.java
@@ -1,8 +1,16 @@
 package com.team.HoneyBadger.Controller;
 
+import com.team.HoneyBadger.DTO.ApprovalResponseDTO;
+import com.team.HoneyBadger.DTO.TokenDTO;
 import com.team.HoneyBadger.Entity.Approver;
+import com.team.HoneyBadger.Exception.DataNotFoundException;
+import com.team.HoneyBadger.Exception.NotAllowedException;
 import com.team.HoneyBadger.Service.MultiService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/approver")
 public class ApproverController {
     private final MultiService multiService;
+
+    @PutMapping // 승인자 상태 변경
+    public ResponseEntity<?> approverStatus(@RequestHeader("Authorization") String accessToken, @RequestHeader("approvalId") Long approvalId) {
+        TokenDTO tokenDTO = multiService.checkToken (accessToken);
+        if (tokenDTO.isOK ()) try {
+            ApprovalResponseDTO approvalResponseDTO = multiService.addReader(approvalId, tokenDTO.username ());
+            return ResponseEntity.status (HttpStatus.OK).body (approvalResponseDTO);
+        } catch (NotAllowedException ex) {
+            return ResponseEntity.status (HttpStatus.FORBIDDEN).body (ex.getMessage ());
+        } catch (DataNotFoundException ex) {
+            return ResponseEntity.status (HttpStatus.NOT_FOUND).body (ex.getMessage ());
+        }
+        else return tokenDTO.getResponseEntity ();
+    }
 
 
 
--- backend/src/main/java/com/team/HoneyBadger/Service/Module/ApproverService.java
+++ backend/src/main/java/com/team/HoneyBadger/Service/Module/ApproverService.java
@@ -5,6 +5,7 @@ import com.team.HoneyBadger.Enum.ApproverStatus;
 import com.team.HoneyBadger.Repository.ApproverRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,7 +15,7 @@ public class ApproverService {
     private final ApproverRepository approverRepository;
 
     public Approver save(SiteUser siteUser, Approval approval){
-        return approverRepository.save (Approver.builder ().user (siteUser).approval (approval).approverStatus (ApproverStatus.READY).build ());
+        return approverRepository.save (Approver.builder ().user (siteUser).approval (approval).approverStatus (ApproverStatus.NOT_READ).build ());
     }
 
     public Approver get(SiteUser user,Approval approval) {
@@ -25,7 +26,14 @@ public class ApproverService {
         return approverRepository.findByApproval (approval);
     }
 
-
+    @Transactional
+    public void updateApproverStatus(Long approverId, ApproverStatus newStatus) {
+        Approver approver = approverRepository.findById (approverId).orElse (null);
+        if (approver != null) {
+            approver.setApproverStatus (newStatus);
+            approverRepository.save (approver);
+        }
+    }
 
 
 }
